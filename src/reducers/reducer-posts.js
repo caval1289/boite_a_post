01c07404d59b30a9c554801ca39d6169af5f7c24
Alessandro Cavaleri <caval1289@gmail.com>
2019-09-17 16:50:07 +0200

    suppression des posts

--- src/reducers/reducer-posts.js
+++ src/reducers/reducer-posts.js
@@ -4,6 +4,15 @@ export default function reducerPosts(state = [], action) {
     switch (action.type) {
         case AT_POSTS.READ_ALL:
             return action.payload;
+
+        case AT_POSTS.DELETE:
+            return state.filter((post) => {
+                if (post.id == action.payload) {
+                    return false
+                } else {
+                    return true
+                }
+            })
     }
     return state;
 }
